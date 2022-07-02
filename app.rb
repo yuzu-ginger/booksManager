09242268fd09fb52c6e7a16392dc2909495dab79
Your Name <you@example.com>
2022-07-02 18:48:27 +0900

    add

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ connect = PG::connect(
 
 get '/' do   # 登録form
     id = @env["QUERY_STRING"].match(/2F/).post_match
-    if id[0] == "index"
+    if id[0] == "A"
         id = id.match(/2F/).post_match.to_i
         erb :index
     else
