59bd8f707744db1e32a13c19b7f8cf90f7ae967b
Your Name <you@example.com>
2022-07-02 19:14:04 +0900

    add

--- app.rb
+++ app.rb
@@ -14,15 +14,11 @@ connect = PG::connect(
 )
 
 get '/' do   # 登録form
+    p id
     id = @env["QUERY_STRING"].match(/2F/).post_match
     if id[0] == "A"
         @id = id.match(/2F/).post_match
         @results = connect.exec("SELECT * FROM books;")
-        @results.each do |result|
-            p result['title']
-            p result['userid']
-            p id
-        end
         erb :index
     else
         @id = id.to_i
