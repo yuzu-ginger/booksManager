37f7ab10bcaefd4d114a10e27c7545b139e4c84b
Your Name <you@example.com>
2022-07-02 15:50:25 +0900

    add pg gem

--- app.rb
+++ app.rb
@@ -25,10 +25,10 @@ def find_id(userid)
         port: "5432"
     )
     results = connect.exec("SELECT * FROM userindex")
-    return reply_id(results)
+    return reply_id(results, userid)
 end
 
-def reply_id(results)   # useridに対応するidを返す.なければ作る
+def reply_id(results, userif)   # useridに対応するidを返す.なければ作る
     results.each do |result|
         if result['userid'] == userid
             return result['id']
